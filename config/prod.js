22fcf7c175f43456afae28f69f4a49fccdd71619
Malmi <gangika6@gmail.com>
2020-05-12 02:46:21 -0400

    auth flow 3

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,5 @@ module.exports = {
     googleClientID:process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     mongoURI: process.env.MONGO_URI,
-    cokieKey: Processing.env.COOKIE_KEY
+    cokieKey: process.env.COOKIE_KEY
 };
